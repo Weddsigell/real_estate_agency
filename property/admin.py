76af271220f99caa5ed96749df99268fd0bcab52
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-03-08 17:43:12 +0500

    сделал поле датой создания объявления неизменяемым в adminui

--- property/admin.py
+++ property/admin.py
@@ -4,6 +4,7 @@ from .models import Flat
 
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
+    readonly_fields = ['created_at']
 
 
 admin.site.register(Flat, AuthorAdmin)
