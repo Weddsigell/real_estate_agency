61594add2bf87e5bdd16d13b7086f8700bca7208
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-03-08 21:17:51 +0500

    добавил поля для отображения в adminui

--- property/admin.py
+++ property/admin.py
@@ -5,6 +5,7 @@ from .models import Flat
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ['created_at']
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
 
 
 admin.site.register(Flat, AuthorAdmin)
