6e888af843c1963c42943060e7f7d0509c8d2cae
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-03-09 21:31:15 +0500

    обновил readme файл
    + некоторые доработки форматирования

--- property/models.py
+++ property/models.py
@@ -8,36 +8,46 @@ class Flat(models.Model):
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
-        db_index=True)
-
-    description = models.TextField('Текст объявления', blank=True)
-    price = models.IntegerField('Цена квартиры', db_index=True)
-
+        db_index=True
+    )
+    description = models.TextField(
+        'Текст объявления',
+        blank=True
+    )
+    price = models.IntegerField(
+        'Цена квартиры',
+        db_index=True
+    )
     town = models.CharField(
         'Город, где находится квартира',
         max_length=50,
-        db_index=True)
+        db_index=True
+    )
     town_district = models.CharField(
         'Район города, где находится квартира',
         max_length=50,
         blank=True,
-        help_text='Чертаново Южное')
+        help_text='Чертаново Южное'
+    )
     address = models.TextField(
         'Адрес квартиры',
-        help_text='ул. Подольских курсантов д.5 кв.4')
+        help_text='ул. Подольских курсантов д.5 кв.4'
+    )
     floor = models.CharField(
         'Этаж',
         max_length=3,
-        help_text='Первый этаж, последний этаж, пятый этаж')
-
+        help_text='Первый этаж, последний этаж, пятый этаж'
+    )
     rooms_number = models.IntegerField(
         'Количество комнат в квартире',
-        db_index=True)
+        db_index=True
+    )
     living_area = models.IntegerField(
         'количество жилых кв.метров',
         null=True,
         blank=True,
-        db_index=True)
+        db_index=True
+    )
     new_building = models.BooleanField(
         'Новостройка',
         null=True,
@@ -49,18 +59,22 @@ class Flat(models.Model):
         null=True,
         blank=True,
     )
-
     has_balcony = models.BooleanField(
         'Наличие балкона',
         null=True,
         blank=True,
-        db_index=True)
-    active = models.BooleanField('Активно-ли объявление', db_index=True)
+        db_index=True
+    )
+    active = models.BooleanField(
+        'Активно-ли объявление',
+        db_index=True
+    )
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
         blank=True,
-        db_index=True)
+        db_index=True
+    )
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
